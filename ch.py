da7f0d66eda186639adb29c312aa2703a90118ec
Jake Carter <jake@cartermade.com>
2015-10-07 14:47:38 -0700

    Lots of cleanup after reviewing code for my Xcoders talk.
    Changed copy/pasted 'pof' function name to 'ch'.
    Cleaned up 'help' text for 'ch' func.
    Added helpful message when 'ch' is called with no arguments.
    Changed string interpolation methods because the old way felt weirder to me than the .format() way.
    Fixed bug that caused an infinite loop when calling -class returned an empty string.

--- ch.py
+++ ch.py
@@ -2,31 +2,33 @@
 
 import lldb
 
-def pof(debugger, command, result, internal_dict):
-	usage = "usage: %prog <instance|class>"
-	'''Prints the class hirarchy for a given className or instance via memeory address. (Ex. 'ch NSString|<memory_address_to_an_NSString>' will output 'NSString > NSObject')
-	'''
-		
+def ch(debugger, command, result, internal_dict):
+	"""Prints the class hirarchy for a given className or instance via memeory address. (Ex. 'ch NSString|<memory_address_to_an_NSString>' will output 'NSString > NSObject')"""
+	
 	ci = debugger.GetCommandInterpreter()
 	ro = lldb.SBCommandReturnObject()
 
 	command = command.strip()
+	
 	if len(command) < 1:
-		print >>result, "Empty argument."
+		print >>result, "Empty argument. Try 'ch NSString' or 'ch <memory_address_to_an_NSString>'"
 		return
-
-	ci.HandleCommand('expr -O -- [%(command)s class]' % {"command" : command}, ro)
+	
+	ci.HandleCommand("expr -l objc++ -O -- [{} class]".format(command), ro)	
 	className = ro.GetOutput().strip()
-
+	if len(className) == 0:
+		print >>result, "Could not get class from {}".format(command)
+		return
+	
 	acc = []
 
 	while className != "nil":
 		acc.append(className)
-		ci.HandleCommand('expr -O -- [%(className)s superclass]' % {"className" : className}, ro)
+		ci.HandleCommand("expr -l objc++ -O -- [{} superclass]".format(className), ro)
 		className = ro.GetOutput().strip()
 
 	print >>result, " > ".join(acc)
 
 def __lldb_init_module(debugger, internal_dict):
-	debugger.HandleCommand('command script add -f ch.pof ch')
+	debugger.HandleCommand('command script add -f ch.ch ch')
 	print 'The "ch" python command has been installed and is ready for use.'
